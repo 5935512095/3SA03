eef7241858e3b75cad45816cb70a6b0ea9a92a10
5935512095 <santepap.chairee@hotmail.com>
2019-09-09 10:02:37 +0700

    Chackpoin

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React ,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Content from './Content';
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard'
 
-let x = 10
+const word = "Santipap";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello {x}
-        <Content />
-      </div>
+    <div>
+    <WordCard value="Santipap"/>
+    </div>
     );
-  }
-}
+   }
+   
+ }
+
 export default App;
